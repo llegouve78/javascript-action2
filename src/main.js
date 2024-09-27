df9cc1f175386a29a21996ea1bceeb6f4b797561
LLG <louis.le-gouvello@dxc.com>
2024-09-27 11:52:39 +0200

    commit after pretier write

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
 const core = require('@actions/core')
-const  github = require("@actions/github")
+const github = require('@actions/github')
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
   try {
-      const token= core.getInput("token")
-      const title = core.getInput("title")
-      const body = core.getInput("body")
-      const assignees = core.getInput("assignees")
+    const token = core.getInput('token')
+    const title = core.getInput('title')
+    const body = core.getInput('body')
+    const assignees = core.getInput('assignees')
 
-      const octokit = github.getOctokit(token)
+    const octokit = github.getOctokit(token)
 
-      const response = await octokit.rest.issues.create({
-        // owner: github.context.repo.owner
-        //
-        ...github.context.repo,
-        title,
-        body,
-        assignees: assignees ? assignees.split('\n'): undefined,
-      })
+    const response = await octokit.rest.issues.create({
+      // owner: github.context.repo.owner
+      //
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split('\n') : undefined
+    })
 
-      core.setOutput("issue", response.data);
-     } catch (error) {
+    core.setOutput('issue', response.data)
+  } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
