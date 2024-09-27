f83aa5e7ebfc6a617759d68ce23bd9ad48634f6f
LLG <louis.le-gouvello@dxc.com>
2024-09-27 11:47:38 +0200

    first update from udemy course

--- src/main.js
+++ src/main.js
@@ -1,25 +1,29 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
-
+const  github = require("@actions/github")
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
   try {
-    const ms = core.getInput('milliseconds', { required: true })
+      const token= core.getInput("token")
+      const title = core.getInput("title")
+      const body = core.getInput("body")
+      const assignees = core.getInput("assignees")
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+      const octokit = github.getOctokit(token)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+      const response = await octokit.rest.issues.create({
+        // owner: github.context.repo.owner
+        //
+        ...github.context.repo,
+        title,
+        body,
+        assignees: assignees ? assignees.split('\n'): undefined,
+      })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
+      core.setOutput("issue", response.data);
+     } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
   }
